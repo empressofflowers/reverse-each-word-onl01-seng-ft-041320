abe24f6fdfd1a9391fa3e3e16b871e391a0e98f9
Devone Austin <devone.austin@gmail.com>
2020-04-17 00:42:44 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+#method `.each` Then utilize the same method using `.collect`
+
+def reverse_each_word(sentence)
+  sentence_one = []
+  sentence.each do |reverse_each_word|
+    sentence_one << sentence.reverse
+    
+  end
+  
+end
