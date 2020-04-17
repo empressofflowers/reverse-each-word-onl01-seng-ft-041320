cf93a91832f74559c22b15148dbb66b4fd5561aa
Devone Austin <devone.austin@gmail.com>
2020-04-17 01:00:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(words)
-    array = words.split
-    new_sentence = []
-    array.each do |word|
-        word.reverse!
-        new_sentence << word
-    end
-    new_sentence.join(" ")
+  backwards = []
+  array = words.split(" ")
+  array.collect do |word|
+  word.reverse!
+  backwards << word
+  end
+  backwards.join(" ")
 end
